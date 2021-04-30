a50c39dbd84c218257d4c1450089c9d9a6a5275f
zany29 <zany0952@gmail.com>
2021-04-30 15:54:17 +0800

    add word count function

--- read.py
+++ read.py
@@ -8,16 +8,56 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完畢, 總共有', len(data), '筆資料')
 
-sum_len = 0
-for d in data:
-	sum_len += len(d)
+print(data[0])
 
-print('連癌的平均長度是', sum_len/len(data))
-
-new = []
+wc = {}  #word_count
 for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆留言長度小於100')
-print(new[0])
-print(new[1])
+	words = d.strip().split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 
+	
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['zany'])
+
+while True:
+	word = input('請輸入想查詢的關鍵字: ')
+	if word == 'q':
+		break
+	if word in wc:	
+		print(word, '出現過的次數為: ', wc[word])
+	else:
+		print('查不到這個字')
+print('感謝您的使用~')
+# sum_len = 0
+# for d in data:
+# 	sum_len += len(d)
+
+# print('連癌的平均長度是', sum_len/len(data))
+
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+# print('一共有', len(new), '筆留言長度小於100')
+# print(new[0])
+# print(new[1])
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('一共有', len(good), '筆留言提到good')
+
+# good = [d for d in data if 'good' in d]
+# print(good)
+
+# bad = ['bad' in d for d in data]
+# print(bad)
+
